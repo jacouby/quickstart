eeed7fb695c8067f1300d911da0c14dd3fa1d06f
jacouby <42520480+jacouby@users.noreply.github.com>
2022-10-25 12:39:14 -0400

    Add files via upload

---
+++ jacouby-quickstart_deb.sh
@@ -0,0 +1,7 @@
+curl -fsSL https://deb.nodesource.com/setup_18.x | bash - &&\
+
+apt install wget nano nodejs python3 python-is-python3 python-dev-is-python3 -y
+
+pip install fastapi aiohttp requests 
+
+npm install -g create-next-app tailwindcss next react
